aa57c92b7ee2fb60ba0285d08fa13f42b8c42fe7
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-09 13:14:00 -0500

    created index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Amanda Williams
+ * January 9, 2019
+ * 328/howdy/index.php
+ */
+
+#Error Reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+#Require autoload
+require_once ('vendor/autoload.php');
+
