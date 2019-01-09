3117cd308c6f89e1627d5193086bd551da04de91
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-09 13:47:04 -0500

    added a view

--- index.php
+++ index.php
@@ -12,3 +12,18 @@ error_reporting(E_ALL);
 #Require autoload
 require_once ('vendor/autoload.php');
 
+#create an instance of the Base class
+$f3 = Base::instance();
+
+#Turn on Fat-Free error reporting/debugging
+$f3->set('DEBUG', 3);
+
+#define a default route
+$f3->route('GET /', function() {
+    //echo '<h1>Hello World!</h1>';
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+#run fat free | has to be the last thing in the index page.
+$f3->run();
